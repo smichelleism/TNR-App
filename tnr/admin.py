2a580201dbd13842573c8869841d7069f8a4a2d1
Sharon Lake <parallaxplace@gmail.com>
2017-11-14 17:04:45 -0800

    Initial commit

---
+++ tnr/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import TNRApplication
+
+# Register your models here.
+
+@admin.register(TNRApplication)
+class TNRApplicationAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('Initial Application Date',
+			{'fields': ['application_date']}),
+		('Applicant Information',
+			{'fields': ['last_name', 'first_name', 'email', 'contact_street_address', 'contact_city', 'contact_zipcode']}),
+		('Colony Address',
+			{'fields': ['colony_cross_streets', 'colony_street_address', 'contact_city', 'colony_zipcode']}),
+		('Colony Information',
+			{#'classes': ('collapse',),
+			'fields': ['cats_total', 'kittens_total', 'cats_friendly', 'cats_pregnant', 'cats_fixed', 'cats_feeding', 'scheduling_issues', 'add_info']}),
+		('KB Notes',
+			{'fields': ['notes']}),
+		]
+
+	list_display = ('application_date', 'first_name', 'last_name', 'colony_street_address', 'cats_total')
+	search_fields = ['last_name', 'colony_street_address']
+	list_filter = ['application_date']
---
+++ tnr/models.py
@@ -0,0 +1,158 @@
+from django.db import models
+from django.utils import timezone
+from datetime import datetime
+
+# Create your models here.
+
+class TNREvent(models.Model):
+    name = models.CharField("Short Description", max_length=50, blank=True)
+    desc = models.CharField("Long Description", max_length=200, blank=True)
+    date = models.DateField(blank=True)
+    def __str__(self):
+        return "%s %s" % (self.name, self.date)
+
+
+class TNRLocation(models.Model):
+    event               = models.ForeignKey(TNREvent, on_delete=models.CASCADE)
+    cp_name             = models.CharField(max_length=200, blank=True)
+    cp_email            = models.CharField(max_length=200, blank=True)
+    cp_telephone        = models.CharField(max_length=200, blank=True)
+    colony_address01    = models.CharField(max_length=200, blank=True)
+    colony_address02    = models.CharField(max_length=200, blank=True)
+    colony_city         = models.CharField(max_length=50, blank=True)
+    colony_zipcode      = models.CharField(max_length=10, blank=True)
+    notes_private       = models.TextField("Public Notes", blank=True)
+    notes_public        = models.TextField("Private Notes. (Not to be shared with CP.)", blank=True)
+    def __str__(self):
+        return "%s %s" % (self.cp_name, self.colony_address01)
+
+
+class TNRLeg(models.Model):
+    location    = models.ForeignKey(TNRLocation, on_delete=models.CASCADE)
+    name        = models.CharField(max_length=50, blank=True)
+    date        = models.DateTimeField(blank=True)
+    def __str__(self):
+        return "%s %s" % (self.name, self.date)
+
+
+class TNRRole(models.Model):
+    leg   = models.ForeignKey(TNRLeg, on_delete=models.CASCADE)
+    name  = models.CharField(max_length=50, blank=True)
+    def __str__(self):
+        return "%s" % (self.cp_name)
+
+
+class Person(models.Model):
+    role       = models.ForeignKey(TNRRole)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name  = models.CharField(max_length=100, blank=True)
+    email      = models.CharField(max_length=200, blank=True)
+    def __str__(self):
+        return "%s %s %s %s" % (self.first_name, self.last_name, self.email, self.telephone)
+
+
+class TNRApplication(models.Model):
+    application_date		= models.DateTimeField(default=timezone.now)
+    first_name              = models.CharField(
+        verbose_name='What is your first name?', 
+        max_length=100)
+    last_name               = models.CharField(
+        verbose_name='What is your family (last) name?', 
+        max_length=100)
+    email                   = models.CharField(
+        verbose_name='What is your email?', 
+        max_length=100)
+    contact_street_address  = models.CharField(
+        verbose_name='What is your home street address?', 
+        max_length=200, 
+        blank=True)
+    contact_city            = models.CharField(
+        verbose_name='What is your home city?', 
+        max_length=50, default="Los Angeles", 
+        blank=True)
+    contact_state           = models.CharField(
+        verbose_name='What state do you live in?',
+        max_length=2, 
+        default="CA", 
+        blank=True)
+    contact_zipcode         = models.CharField(
+        verbose_name='What is your home zip code?', 
+        max_length=10, 
+        blank=True)
+    contact_phone_cell		= models.CharField(
+        verbose_name='What is your cell phone number?', 
+        max_length=20, 
+        blank=True)
+    contact_phone_land		= models.CharField(
+        verbose_name='What is your home telephone number?', 
+        max_length=20, 
+        blank=True)
+    colony_street_address   = models.CharField(
+        verbose_name='What is the street address for the colony?', 
+        max_length=200, 
+        blank=True)
+    colony_cross_streets    = models.CharField(
+        "What are the major cross streets nearest the colony?",
+        max_length=200, 
+        blank=True)
+    colony_city             = models.CharField(
+        verbose_name='What city is the colony located?', 
+        max_length=50, 
+        default="Los Angeles", 
+        blank=True)
+    colony_state            = models.CharField(
+        max_length=2, 
+        default="CA", 
+        blank=True)
+    colony_zipcode          = models.CharField(
+        verbose_name='What is the zip code where the colony is located?', 
+        max_length=10, 
+        blank=True)
+    transportation          = models.CharField(
+        verbose_name='Do you have a car or other transportation to help with taking the cats to/from the vet?', 
+        max_length=20,
+        blank=True)
+    location_type           = models.CharField(
+        verbose_name='Please describe the colony location. Is it a business/home/school/field/or ...', 
+        max_length=10, 
+        blank=True)
+    cats_total              = models.CharField(
+        verbose_name='How many cats total (including kittens)?', 
+        max_length=50, 
+        blank=True)
+    kittens_total           = models.CharField(
+        verbose_name='How many kittens are there? What is their approximate age?', 
+        max_length=50, 
+        blank=True) 
+    cats_friendly           = models.CharField(
+        verbose_name='Are any of the cats friendly? Can you touch them?', 
+        max_length=100, 
+        blank=True)
+    cats_pregnant           = models.CharField(
+        verbose_name='Do you know if any of the cats are currently pregnant?', 
+        max_length=100, 
+        blank=True)
+    cats_fixed              = models.CharField(
+        verbose_name='Do you know if any of the cats are already fixed?', 
+        max_length=100, 
+        blank=True)
+    cats_feeding            = models.CharField(
+        verbose_name='Who feeds the cats? What time are they normall fed (morning/evening)?', 
+        max_length=200, 
+        blank=True)
+    scheduling_issues       = models.CharField(
+        verbose_name='The community partner needs to be present to show us where the cats hang out. Any scheduling issues which might cause issues?', 
+        max_length=200, 
+        blank=True)
+    add_info                = models.TextField(
+        verbose_name='Is there any additional information which would helpful?', 
+        blank=True)
+    notes                   = models.TextField(
+        verbose_name='Private KB Notes about location.', 
+        blank=True)
+    app_status              = models.TextField(
+        verbose_name="Application Status",
+        blank=True)
+
+    def __str__(self):
+        return "%s %s %s %s %s" % (self.first_name, self.last_name, self.colony_street_address, self.cats_total. self.app_status)
