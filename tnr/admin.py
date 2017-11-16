5a1c74b7aa0fd9e2a303cbbf608c6063ccc75830
Sharon Lake <parallaxplace@gmail.com>
2017-11-16 00:50:22 -0800

    App Status Dropdown working

--- tnr/admin.py
+++ tnr/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import TNRApplication
 
-# Register your models here.
 
+# Register your models here.
 
 @admin.register(TNRApplication)
 class TNRApplicationAdmin(admin.ModelAdmin):
